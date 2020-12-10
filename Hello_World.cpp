83cdfa4c6e4fa9f89ed047a18d32aa6727c0db0d
khushi-kothari <62203565+khushi-kothari@users.noreply.github.com>
2020-12-10 19:52:17 +0530

    Getting started with c++ :D

---
+++ Hello_World.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    
+    cout<<"Hello World!"<<'\n';
+    
+    //cout<<"Hello World!"<<endl;
+    //endl means to end that particular line
+    
+    
+    return 0;
+}
